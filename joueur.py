d3ac877f2688fe5cdcb7d0449674088acaa64151
Mehdi DELVAUX <mehdi.delvaux@igtp4-mac06.isim.intra>
2016-11-24 13:03:02 +0100

    node js

---
+++ joueur.py
@@ -0,0 +1,34 @@
+
+class Joueur:
+	
+	def __init__(self,Ensbateaux,actif):
+		
+
+	def ensbateaux(self): #ensbateaux : 
+		
+
+	def getActif(self):
+		
+
+	def setEnsbateaux(self,bateaux):
+		
+
+	def setActif(self,actif):
+		
+
+	def toursuivant(self):
+		
+
+ 	def perdu(self):
+ 		
+
+	
+
+
+
+
+
+		
+		
+
+
---
+++ resultat.py
@@ -0,0 +1,50 @@
+
+
+class Resultat : 
+	def __init__(self,joueur,tir): 	# creer_resultat : Joueur x Tir -> Resultat
+									# Creation d'un type resultat qui prend en paramètre le joueur adverse ainsi que le tir du joueur.
+
+		
+
+	
+	def joueur(self): # joueur : Resultat -> Joueur
+					  # Renvoie le joueur a qui appartient le resultat.
+
+
+					  #Propiétés du type 
+					  #L1 : Le joueur est forcement inactif au moment de l'appel de resultat. 
+
+		
+	
+	def tir(self): 	 # tir : Resultat -> Tir 
+					 # Renvoie le tir du joueur.
+	
+
+
+	def resultat_tir(self, tir): # resultat_tir : Resultat -> string 
+								 # Renvoie une chaine de caractère pouvant correspondre a (la chaine sera ensuite affiché dans le main, l'affichage ne se fait pas ici !) :
+								 	# - Si le tir correspond extactement a l'une des positions d'un des bateaux du joueur adverse (verifier pour chaque bateau dans leur ensemble de position -> fonction dans la classe bateau) ALORS	
+								 			#Si la position n'a pas deja été touché
+								 				#	>>> RENVOIE "Touché"" <<<
+								 				# Si apres toute les positions du bateaux on été touché ALORS
+								 					# >>> RENVOIE "coulé "<<<
+
+								 			# - Sinon 
+								 				# >>> RENVOIE "a l'eau "<<<
+
+
+								 	# - Sinon Si le tir a touché l'une des deux coordonnées d'une position d'un bateau du joueur adverse ALORS
+								 			# >>> RENVOIE "En Vue" <<<
+								 	# - Sinon 
+								 			# >>> RENVOIE "a l'eau" <<<
+
+								 	
+
+
+	
+
+		
+
+
+
+				
