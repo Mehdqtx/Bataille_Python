7b12ca9be8e9a21d943745f84e36ed74c8cd027e
mehdi delvaux <mehdi.delvaux@gmail.com>
2016-11-28 19:16:44 +0100

    joueur.py

--- joueur.py
+++ joueur.py
@@ -1,25 +1,31 @@
 
 class Joueur:
 	
-	def __init__(self,Ensbateaux,actif):
+	def __init__(self,Ensbateaux,actif): #creer_joueur : Ensbateaux x Bool -> Joueur
+										 #Creation d'un joueur qui prend en paramètre l'ensemble de bateaux du joueur et un boolean qui permet de savoir si c'est au tour du joueur de jouer (if TRUE)
+										 
+
+	def ensbateaux(self): #ensbateaux : Joueur -> Ensbateaux
+						  # Renvoie l'ensemble de bateaux du joueur
 		
 
-	def ensbateaux(self): #ensbateaux : 
+	def etat(self): #etat : Joueur -> Bool
+					# Renvoie l'etat dans lequel se trouve le joueur actuellement.
+					# True s'il est actif, False sinon
 		
 
-	def getActif(self):
+	def modif_Ensbateaux(self,bateaux): 
 		
 
-	def modif_Ensbateaux(self,bateaux):
+	def modif_etat(self): #modif_etat : Joueur -> Joueur
+						  # Renvoie True si le joueur etait dans l'etat False
+						  # Renvoie False si le joueur etait dans l'etat True
+
+	def toursuivant(self): 
 		
 
-	def modif_etat(self):
-		
-
-	def toursuivant(self):
-		
-
- 	def perdu(self):
+ 	def perdu(self): # perdu : Joueur -> Bool
+ 					 # Renvoie True si le joueur a perdu c'est a dire que tout ses bateaux ont été touchés.
  		
 
 	
