78a001245f7eff1e38fc596c02d6dc263162816c
Mehdi DELVAUX <mehdi.delvaux@igtp4-mac06.isim.intra>
2016-11-29 13:17:10 +0100

    c'est de la merde

--- position.py
+++ position.py
@@ -1,35 +1,37 @@
-
+#-*- coding: utf-8 -*-
 class Position:
 	def __init__(self,coordonneeX,coordonneeY): # creer_position : int * int -> Positiion
 												# Constructeur du type position. Un bateau est composé d'une ou plusieurs positions
-
+		return
 
 	def absice(self): 	# absice : int * int -> Position 
-						# permet de recuperer l'absice d'une position
+		return			# permet de recuperer l'absice d'une position
 						# absice(creer_position(a,b))=a
 						# ordonnee(creer_position(a,b))= b
 						# toucher(creer_position(a,b)) = false
 
 	def ordonnee(self):	# ordonnee : Position -> int
-						# permet de recuperer l'ordonnée d'une position
+		return			# permet de recuperer l'ordonnée d'une position
 
 	def toucher(self):	# toucher : Position -> bool
-						# permet de recuperer l'etat d'une position à savoir si il est touché ou pas
+		return			# permet de recuperer l'etat d'une position à savoir si il est touché ou pas
 
-	def modif_toucher(self):# modif_toucher : Position -> bool
-							# Permet de modifier l'état de la position. Si la position est à touché elle ne doit plus etre modifiée sinon on la passe à TRUE
+	def modif_toucher(self): # modif_toucher : Position -> bool
+		return				 # Permet de modifier l'état de la position. Si la position est à touché elle ne doit plus etre modifiée sinon on la passe à TRUE
 
 	def est_valide_position(self): # verifie si la position crée est a l'intérieur des limites imposées par le sujet : 20 case en absices, 20 cases en ordonnee
-									# est_valide_position(p : position) == true ==> 0<absice(p)<=20 et 0<ordonnee(p)<=20
+		return					   # est_valide_position(p : position) == true ==> 0<absice(p)<=20 et 0<ordonnee(p)<=20
 
 
 
-	#Test unitaire
-	p = position(1,2);
-	pos = position(3,25);
-	pos1 = position(25,3);
-	p.absice();
-	p.ordonnee();
-	pos.est_valide_position();
-	pos1.est_valide_position();
-	p.toucher();
+
+#Test unitaire
+
+p = Position(1,2);
+pos = Position(3,25);
+pos1 = Position(25,3);
+p.absice();
+p.ordonnee();
+pos.est_valide_position();
+pos1.est_valide_position();
+p.toucher();
